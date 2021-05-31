49307447fdb5b092bc0e624a16c73249e8d16109
alexkemboi4127 <alexkemboi97@yahoo.com>
2021-05-31 17:02:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+class Calculator{
+constructor(previousOperandTextElement,currentOperandTextElement){
+    this.previousOperandTextElement=previousOperandTextElement;
+    this.currentOperandTextElement=currentOperandTextElement;
+    this.clear()
+}
+clear(){
+this.currentOperand=''
+this.previousOperand=''
+this.operation=undefined
+}
+delete(){
+
+}
+appendNumber(number){
+this.currentOperand=number
+console.log(this.currentOperand)
+}
+chooseOperation(operation){
+
+}
+compute(){
+
+}
+updateDisplay(){
+this.currentOperandTextElement.innerText=this.currentOperand
+}
+}
+
+
+
+const numberButtons=document.querySelectorAll('[data-number]')
+const operationButtons=document.querySelectorAll('[data-operation]')
+const equalsButtons=document.querySelectorAll('[data-equals]')
+const deleteButtons=document.querySelectorAll('[data-delete]')
+const allClearButtons=document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement=document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement=document.querySelectorAll('[data-current-operand ]')
+
+
+const calculator=new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+}) 
